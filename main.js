d5cc30714e5c0abb1f4c9b2175947cb2bd68b792
Marc Farra <marcfarra@gmail.com>
2015-12-07 01:19:24 -0500

    Add marked

--- main.js
+++ main.js
@@ -3,11 +3,25 @@
 Documents = new Mongo.Collection('Documents');
 if (Meteor.isClient) {
 
+  var $editor, $rendered = {};
   if (Documents.find().count() === 0) {
     Documents.insert({docid: "1"});
   }
+  Meteor.startup(function () {
+    $editor = document.getElementById('editor');
+    $rendered = document.getElementById('rendered');
+
+    $rendered.innerHTML = marked($editor.value);
+  })
 
   Template.body.helpers({
     docid: Documents.findOne().docid
+  });
+
+  Template.body.events({
+    'keyup textarea': function (event) {
+      var textValue = $editor.value;
+      $rendered.innerHTML = marked(textValue);
+    }
   });
 }
